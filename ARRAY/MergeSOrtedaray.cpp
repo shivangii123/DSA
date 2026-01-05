bb1f81b4d6e25ffbae155d7bc1868d09f64da739
shivangii123 <shivangigupta2801@gmail.com>
2026-01-05 15:16:42 +0530

    update

--- ARRAY/MergeSOrtedaray.cpp
+++ ARRAY/MergeSOrtedaray.cpp
@@ -47,12 +47,14 @@
 //     return 0;
 // }
 
+
+// arr1= {5,8,9}  arr2={ 3,7,10}  = {3,5,7,8,9,10}
 #include<iostream>
 using namespace std;
 
 int main(){
 
-    int m,n,T;
+    int m,n ;
     cout<<"enter array 1 size : " ; cin>>m;
     cout<<"enter array 2 size :" ;  cin>> n;
 
@@ -62,26 +64,24 @@ int main(){
     for(int i=0;i<m;i++){
         cin>>arr1[i] ;
     }
-    cout<<"arary 1 is : " ;
+    cout<<"Arary 1 is : " ;
     for(int i=0;i<m+n;i++){
         cout<<arr1[i]<<" " ;
     }
 
-    cout<<"Enter array2 elements :" ;
+    cout<<"\nEnter array2 elements :" ;
     for(int i=0;i<n;i++){
         cin>>arr2[i];
     }
 
-    int i=m-1 , j=n-1 ;
-    for(int k=m+n-1 ;k>=0 ; k--){
+    int i=m-1 , j=n-1 , k= m+n-1;
+ 
         while(i>=0 && j>=0){
             if(arr1[i]>=arr2[j]){
-                arr1[k]=arr1[i];
-                i-- ;
+                arr1[k--]=arr1[i--];
             }
             else{
-                arr1[k]=arr2[j];
-                j-- ;
+                arr1[k--]=arr2[j--];
             }
         }
         while(i>=0){
@@ -90,7 +90,6 @@ int main(){
         while(j>=0){
             arr1[k--]=arr2[j--];
         }
-    }
 
     cout<<"Merged sorted array is : ";
     for(int i=0;i<n+m ;i++){
