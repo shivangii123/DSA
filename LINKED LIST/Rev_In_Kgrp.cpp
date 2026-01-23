ec23f0f445c8144d6b3afb10e5bd1a29b67a7060
shivangii123 <shivangigupta2801@gmail.com>
2026-01-23 18:27:14 +0530

    add files

--- LINKED LIST/Rev_In_Kgrp.cpp
+++ LINKED LIST/Rev_In_Kgrp.cpp
@@ -23,89 +23,89 @@ void printLL(Node *head){
     cout<<"NULL " ;
 }
 
-Node* rev_in_Kgrp(Node *head, int k){
-    if(head== NULL || k<=1 ) return head ;
+//*************M-1 Find remaing Length***************//
+int lengthLL(Node *h){
+    int cnt = 0;
+    while(h != NULL){
+        cnt++;
+        h = h->next ;
+    }
+    return cnt;
+}
 
-    Node *dummy = new Node(-1); //insert dummy at start of LList
-    dummy->next = head ;  // if l=1,(head changes);no need to handle separately
-
-    Node *p= dummy , *tail = head ;
-    
-    while(true) {// if k nodes is there, only then Reverse
-        tail = head ;
-        for(int i =1; i<k && tail!= NULL; i++){ // Place Tail at correct posn
-            tail = tail->next ; //moving tail n checking it doesn't becomes NULL
-        }
-        
-        if(tail ==NULL) { break ; }
-
-        Node *n =tail->next ; // To pevent Mry Leak
-
-        Node *curr = head, *prev = NULL ,*nxt; //Reverse K- Nodes
-        for(int i=0; i<k; i++){ 
-            nxt = curr-> next ;
-            curr->next = prev;
-            prev = curr;
-            curr = nxt ;
-        }
-        p ->next = tail ;   //Re-wire
-        head ->next = n ;
-
-        p = head ; //move pointers
-        head = n ;
+Node *rev_in_Kgrp(Node *head, int k){
+    if(head == NULL || k<=1 ){
+        return head ;
     }
 
-    Node *res = dummy ->next ;
-    delete dummy ;
+    Node *dummy = new Node(-1);
+    dummy->next = head ; // connect dummy to the LList
+
+    Node *prvGrp = dummy ;
+    int remainingLen = lengthLL(head) ;
+    while(remainingLen >=k){
+
+        Node *tail = head ; // Place tail at corect Position
+        for(int i=1;i<k;i++){
+            tail= tail->next;
+        }
+        remainingLen -= k ;
+        
+        Node *nxtgrp = tail->next ;
+        
+        Node *curr = head, *prev = NULL, *nxt ; // Reverse K Nodes
+        for(int i=0;i<k;i++){
+            nxt = curr->next ;
+            curr->next = prev ;
+            prev = curr ;
+            curr = nxt ;
+        }
+
+        prvGrp->next = tail ; // Re-Connect Sublist to main List
+        head->next = nxtgrp ;
+        prvGrp = head ;// Move pointers to next grp
+        head = nxtgrp ; 
+    }
+    Node *res = dummy->next;
+    delete dummy;
     return res ;
 }
 
+//************M-2 Use while + break when less than k nodes left ;(same T.C) ***********//
+// Node* rev_in_Kgrp(Node *head, int k){
+//     if(head== NULL || k<=1 ) return head ;
 
-//*************M-2 same time , Find remaing Length***************//
-// int lengthLL(Node *h){
-//     int cnt = 0;
-//     while(h != NULL){
-//         cnt++;
-//         h = h->next ;
-//     }
-//     return cnt;
-// }
+//     Node *dummy = new Node(-1); //insert dummy at start of LList
+//     dummy->next = head ;  // if l=1,(head changes);no need to handle separately
 
-// Node *rev_in_Kgrp(Node *head, int k){
-//     if(head == NULL || k<=1 ){
-//         return head ;
-//     }
-
-//     Node *dummy = new Node(-1);
-//     dummy->next = head ; // connect dummy to the LList
-
-//     Node *prvGrp = dummy ;
-//     int remainingLen = lengthLL(head) ;
-//     while(remainingLen >=k){
-
-//         Node *tail = head ; // Place tail at corect Position
-//         for(int i=1;i<k;i++){
-//             tail= tail->next;
+//     Node *p= dummy , *tail = head ;
+    
+//     while(true) {// if k nodes is there, only then Reverse
+//         tail = head ;
+//         for(int i =1; i<k && tail!= NULL; i++){ // Place Tail at correct posn
+//             tail = tail->next ; //moving tail n checking it doesn't becomes NULL
 //         }
-//         remainingLen -= k ;
         
-//         Node *nxtgrp = tail->next ;
-        
-//         Node *curr = head, *prev = NULL, *nxt ; // Reverse K Nodes
-//         for(int i=0;i<k;i++){
-//             nxt = curr->next ;
-//             curr->next = prev ;
-//             prev = curr ;
+//         if(tail ==NULL) { break ; }
+
+//         Node *n =tail->next ; // To pevent Mry Leak
+
+//         Node *curr = head, *prev = NULL ,*nxt; //Reverse K- Nodes
+//         for(int i=0; i<k; i++){ 
+//             nxt = curr-> next ;
+//             curr->next = prev;
+//             prev = curr;
 //             curr = nxt ;
 //         }
+//         p ->next = tail ;   //Re-wire
+//         head ->next = n ;
 
-//         prvGrp->next = tail ; // Re-Connect Sublist to main List
-//         head->next = nxtgrp ;
-//         prvGrp = head ;// adance grp pointers
-//         head = nxtgrp ; 
+//         p = head ; //move pointers
+//         head = n ;
 //     }
-//     Node *res = dummy->next;
-//     delete dummy;
+
+//     Node *res = dummy ->next ;
+//     delete dummy ;
 //     return res ;
 // }
 
