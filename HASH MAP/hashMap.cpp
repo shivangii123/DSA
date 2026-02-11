03e8db6205a52668b065bd683fae324b5255f7c8
shivangii123 <shivangigupta2801@gmail.com>
2026-02-11 13:12:27 +0530

    hash map basics

---
+++ HASH MAP/hashMap.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+#include<unordered_map>
+using namespace std ;
+
+int main(){
+    unordered_map<string, int>mp;
+
+    mp["John"]=123457 ;
+    mp["Jack"]=7564532;
+
+    cout<<"value is :"<<mp["Jack"]<<endl ;
+
+
+}
